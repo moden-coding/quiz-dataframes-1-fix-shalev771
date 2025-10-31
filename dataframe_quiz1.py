6c763ba9cfadcda376ff703026697ba8f127009b
shalev771 <28sdayspinowitz@gannacademy.org>
2025-10-31 09:41:21 -0400

    done

--- dataframe_quiz1.py
+++ dataframe_quiz1.py
@@ -1,22 +1,26 @@
 import pandas as pd
 
 def get_department(df):
-    pass
+    return df.loc[:,"Department"]
+    
     
 def get_second_record(df):
-    pass
+    return df.iloc[2,:]
+    
+
     
 def get_info_grace(df):
-    pass
+    return df.loc["Grace",:]
 
 def get_experience_location(df):
-    pass
+    
+    return df.loc[:,["Experience", "Location"]]
     
 def get_department_elena_aiden(df):   
-    pass
-
+    
+    return df.loc[["Elena","Aiden"],["Department"]]
 def get_experience_salary_third_to_end(df):
-    pass
+    return df.iloc[2:,[1,3]]
 
 def main():
     df = pd.read_csv("data.csv", index_col= 0)
